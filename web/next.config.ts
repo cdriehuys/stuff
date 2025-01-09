c2bd3968b200b634f2044f620d0d2d1f7d00e4bd
Chathan Driehuys <chathan@driehuys.com>
2025-01-09 18:28:40 -0500

    feat(web): List vendors (#7)

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Output in standalone mode for containerized app
-  output: "standalone"
+  output: "standalone",
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/:path*",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
