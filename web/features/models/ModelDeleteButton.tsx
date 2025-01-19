fbf17c6b9c330626b110fd24c41eb0f4e4e0c6e5
Chathan Driehuys <chathan@driehuys.com>
2025-01-19 16:08:19 +0000

    feat(web): Handle model deletion errors

--- web/features/models/ModelDeleteButton.tsx
+++ web/features/models/ModelDeleteButton.tsx
@@ -7,15 +7,26 @@ import { IconTrash } from "@tabler/icons-react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { redirect } from "next/navigation";
 import { modelKeys } from "./queries";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
+import { asAPIError } from "@/api/errors";
+import ErrorOverlay from "../errors/ErrorOverlay";
 
 interface Props {
   model: components["schemas"]["Model"];
 }
 
 export default function ModelDeleteButton({ model }: Props) {
+  const [error, setError] = useState<components["schemas"]["APIError"]>({});
+  const [opened, { open, close }] = useDisclosure(false);
+
   const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: () => browserClient.deleteModelByID(model.id),
+    onError: (error) => {
+      setError(asAPIError(error));
+      open();
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: modelKeys.lists() });
       queryClient.setQueryData(modelKeys.detail(model.id), undefined);
@@ -27,14 +38,22 @@ export default function ModelDeleteButton({ model }: Props) {
   }
 
   return (
-    <Button
-      color="red"
-      leftSection={<IconTrash />}
-      loading={mutation.isPending}
-      onClick={() => mutation.mutate()}
-      size="compact-md"
-    >
-      Delete
-    </Button>
+    <>
+      <ErrorOverlay
+        error={error}
+        onClose={close}
+        opened={opened}
+        title="Can't Delete Model"
+      />
+      <Button
+        color="red"
+        leftSection={<IconTrash />}
+        loading={mutation.isPending}
+        onClick={() => mutation.mutate()}
+        size="compact-md"
+      >
+        Delete
+      </Button>
+    </>
   );
 }
